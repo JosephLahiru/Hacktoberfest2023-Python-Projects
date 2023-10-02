9ecc253185ac4c59c93f18a9a84275f45f9e0d4d
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-02 17:33:20 +0530

    Create radixSort.py

---
+++ KoshikaDul1999/radixSort.py
@@ -0,0 +1,33 @@
+def radix_sort(arr):
+    max_num = max(arr)
+    exp = 1  
+
+    while max_num // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10 
+
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr)
+print("Sorted array:", arr)
